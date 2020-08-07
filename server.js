3d6191f0fb8096b6367c7d8716cd781388f70981
Richard Rosario <tech2js@gmail.com>
2020-08-07 15:42:40 +0800

    install postman to check api endpoints

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const http = require("http");
+// data sample
+const todos = [
+  { id: 1, text: "todo1" },
+  { id: 2, text: "todo2" },
+  { id: 3, text: "todo3" },
+];
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end();
+  res.setHeader("Content-type", "application/json");
+  res.setHeader("X-Powered-By", "Nodejs");
+  res.write("Hello world");
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 
 const PORT = 5080;
 
 server.listen(PORT, () => console.log(`Serve running at port ${PORT} `));
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
+});
